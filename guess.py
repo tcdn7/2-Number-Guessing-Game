ee87c74d41c3c629b003fce1312d0bfeaf3da80e
Tacdin Özmen <tcdnozmn7@gmail.com>
2025-10-21 15:34:27 +0300

    feat: looped guessing with attempts counter

--- guess.py
+++ guess.py
@@ -1,13 +1,19 @@
 import random
 
 secret=random.randint(1, 100)
-guess=int(input("Enter your guess (1-100): "))
+attempts=0
 
-if guess < secret:
-    print("Too low!")
-elif guess > secret:
-    print("Too high!")
-else:
-    print("Correct!")
+while True:
+    guess=int(input("Enter your guess (1-100): "))
+    attempts+=1
+
+    if guess < secret:
+        print("Too low!")
+    elif guess > secret:
+        print("Too high!")
+    else:
+        print("Correct!")
+        print(f"You found it in {attempts} attempts.")
+        break
 
 # print(secret)  # For testing only
